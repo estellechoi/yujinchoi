25920dfb155249e93582b39a3bdb8b1ecfaedf65
Estelle Choi <estele.choi@gmail.com>
2022-01-12 17:23:22 +0900

    config: eslint + prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // https://github.com/prettier/eslint-plugin-prettier#options
+    "prettier/prettier": [
+      "error", 
+      {}, 
+      { usePrettierrc: true, }
+    ],
   },
   overrides: [
     {
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'strict',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    proseWrap: 'never',
+    quoteProps: 'as-needed',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: true,
+    vueIndentScriptAndStyle: true,
+}
