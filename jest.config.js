67d24a1e304f6713dc36746cedeede878bcdf45a
Estelle Choi <estele.choi@gmail.com>
2022-01-12 23:54:41 +0900

    lint: files linted

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ["**/*.{ts,vue}", "!**/node_modules/**", "!**/coverage/**", "!**/tests/**"],
-};
+	preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+	transform: {
+		'^.+\\.vue$': 'vue-jest',
+	},
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'**/*.{ts,vue}',
+		'!**/node_modules/**',
+		'!**/coverage/**',
+		'!**/tests/**',
+	],
+}
