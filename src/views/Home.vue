80109bc655ef7111241a427d15a7bc6b0b033dc2
Estelle Choi <estele.choi@gmail.com>
2022-01-15 05:43:23 +0900

    feature: Home got Clock & state mutating method is called here every second

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,74 @@
 <template>
-	<div class="home">
-		<HourHand :hour="1" />
+	<div :class="$style.layout">
+		<Clock
+			:class="$style.clock"
+			:hours="getDisplayHours"
+			:minutes="getDisplayMinutes"
+			:seconds="getDisplaySeconds"
+		/>
 	</div>
 </template>
 
 <script lang="ts">
 	import { defineComponent } from 'vue'
-	import HourHand from '@/components/HourHand.vue' // @ is an alias to /src
+	import { mapGetters } from 'vuex'
+	import Clock from '@/components/clock/Clock.vue'
 
 	export default defineComponent({
 		name: 'Home',
-		components: {
-			HourHand,
+		components: { Clock },
+		computed: {
+			...mapGetters('date', [
+				'getDisplayHours',
+				'getDisplayMinutes',
+				'getDisplaySeconds',
+			]),
+		},
+		created() {
+			this.startClockInterval()
+		},
+		methods: {
+			startClockInterval() {
+				window.setInterval(() => {
+					this.updateDisplayDateTime()
+				}, 1000)
+			},
+			updateDisplayDateTime() {
+				this.$store.commit(
+					'date/SET_DISPLAY_DATE_TIME',
+					this.getCurrentKorDateTime()
+				)
+			},
+			getCurrentKorDateTime(): Date {
+				const now = new Date()
+				const utcNow = now.getTime() + now.getTimezoneOffset() * 60 * 1000
+				const KOR_TIME_DIFF = 9 * 60 * 60 * 1000
+				const korNow = new Date(utcNow + KOR_TIME_DIFF)
+				return korNow
+			},
 		},
 	})
 </script>
+
+<style lang="scss" module>
+	@import '@/styles/index';
+
+	$clock-size: $size-4 * 10;
+	$clock-hand-size: $size-1;
+
+	.layout {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.clock {
+			width: $clock-size;
+			height: $clock-size;
+			font-size: $clock-hand-size;
+			margin: 0 auto;
+			transform: translateY(-$size-6);
+		}
+	}
+</style>
